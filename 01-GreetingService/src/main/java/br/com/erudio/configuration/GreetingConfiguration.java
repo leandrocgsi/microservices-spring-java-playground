b61a2344022b1310643682f78f31eb8bbd2c9403
leandrocgsi <leandrocgsi@gmail.com>
2021-04-07 21:52:44 -0300

    Initial commit

---
+++ 01-GreetingService/src/main/java/br/com/erudio/configuration/GreetingConfiguration.java
@@ -0,0 +1,31 @@
+package br.com.erudio.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("greeting-service")
+public class GreetingConfiguration {
+	
+	private String greeting;
+	private String defaultValue;
+
+	public GreetingConfiguration() {
+	}
+
+	public String getGreeting() {
+		return greeting;
+	}
+
+	public void setGreeting(String greeting) {
+		this.greeting = greeting;
+	}
+
+	public String getDefaultValue() {
+		return defaultValue;
+	}
+
+	public void setDefaultValue(String defaultValue) {
+		this.defaultValue = defaultValue;
+	}
+}
