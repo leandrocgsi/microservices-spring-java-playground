9cc275f7dcff0e767181f65f3e53062ab7bfd81c
leandrocgsi <leandrocgsi@gmail.com>
2021-04-09 14:40:17 -0300

    Working with Spring Cloud Configuration and Spring Boot Actuator

--- 01-GreetingService/src/main/java/br/com/erudio/configuration/GreetingConfiguration.java
+++ 01-GreetingService/src/main/java/br/com/erudio/configuration/GreetingConfiguration.java
@@ -1,9 +1,11 @@
 package br.com.erudio.configuration;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Component
+@RefreshScope
 @ConfigurationProperties("greeting-service")
 public class GreetingConfiguration {
 	
