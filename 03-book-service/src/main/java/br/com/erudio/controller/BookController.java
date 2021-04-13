0ddc4aef633c64f55cc03a74292c47f413a20c04
leandrocgsi <leandrocgsi@gmail.com>
2021-04-13 17:01:32 -0300

    Creating and Connecting Microservices with Spring Cloud and Feign

--- 03-book-service/src/main/java/br/com/erudio/controller/BookController.java
+++ 03-book-service/src/main/java/br/com/erudio/controller/BookController.java
@@ -1,13 +1,17 @@
 package br.com.erudio.controller;
 
+import java.util.HashMap;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import br.com.erudio.model.Book;
+import br.com.erudio.model.response.Cambio;
 import br.com.erudio.repository.BookRepository;
 
 @RestController
@@ -27,7 +31,22 @@ public class BookController {
 		String port = environment.getProperty("local.server.port");
 		
 		Book book = repository.getById(id);
+		
+		HashMap<String, String> params = new HashMap<>();
+		
+		params.put("amount", book.getPrice().toString());
+		params.put("from", "USD");
+		params.put("to", currency);
+			
+		var response = new RestTemplate()
+			.getForEntity("http://localhost:8000/cambio-service/"
+					+ " {amount}/{from}/{to}",
+					Cambio.class, params);
+		
+		Cambio cambio = response.getBody();
 		book.setEnvironment(port);
+		book.setPrice(cambio.getConvertedValue());
+		book.setCurrency(currency);
 		return book;
 	}
 }
