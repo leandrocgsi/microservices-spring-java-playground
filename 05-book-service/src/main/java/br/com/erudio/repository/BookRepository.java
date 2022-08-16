1735d8680a2f1f04a6b22d768ce1dd6b90850111
leandrocgsi <leandrocgsi@gmail.com>
2022-08-16 18:38:02 -0300

    Branch cleanup

--- 05-book-service/src/main/java/br/com/erudio/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.erudio.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.erudio.model.Book;
-
-public interface BookRepository extends JpaRepository<Book, Long>{}
