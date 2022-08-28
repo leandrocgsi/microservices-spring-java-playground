84ef545834a19226dca78f094889a1b5068a6dc0
leandrocgsi <leandrocgsi@gmail.com>
2022-08-28 18:41:42 -0300

    Distributed Tracing with Docker, Zipkin, Zookeeper and Sleuth

---
+++ 15_DistributedTracingDockerZipkinZookeeperSleuth/book-service/src/main/java/br/com/erudio/controller/BookController.java
@@ -0,0 +1,46 @@
+package br.com.erudio.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.erudio.model.Book;
+import br.com.erudio.proxy.CambioProxy;
+import br.com.erudio.repository.BookRepository;
+
+@RestController
+@RequestMapping("book-service")
+public class BookController {
+	
+	@Autowired
+	private Environment environment;
+	
+	@Autowired
+	private BookRepository repository;
+	
+	@Autowired
+	private CambioProxy proxy;
+	
+	@GetMapping(value = "/{id}/{currency}")	
+	public Book findBook(
+			@PathVariable("id") Long id,
+			@PathVariable("currency") String currency
+			) {
+		
+		var book = repository.getById(id);
+		if (book == null) throw new RuntimeException("Book not Found");
+				
+		var cambio = proxy.getCambio(book.getPrice(), "USD", currency);
+		
+		var port = environment.getProperty("local.server.port");
+		//book.setEnvironment(port + " FEIGN");
+		book.setEnvironment(
+				"BOOK port: " + port + 
+				" CAMBIO port: " + cambio.getEnvironment());
+		book.setPrice(cambio.getConvertedValue());
+		return book;
+	}
+}
